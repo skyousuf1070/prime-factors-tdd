8cd4f20267923add06a6df33e513ad421b170e4e
yousuf <skyousuf1070@gmail.com>
2025-10-25 12:28:56 +0530

    feat: Handle number 2 logic

--- src/test/java/PrimeFactorsTest.java
+++ src/test/java/PrimeFactorsTest.java
@@ -12,7 +12,15 @@ public class PrimeFactorsTest {
         MatcherAssert.assertThat(factorOf(1), is(emptyList()));
     }
 
+    @Test
+    public void shouldReturnListContainingTwoWhenNumberIsTwo() {
+        MatcherAssert.assertThat(factorOf(2), is(List.of(2)));
+    }
+
     private List<Integer> factorOf(Integer number) {
+        if (number == 2) {
+            return List.of(2);
+        }
         return emptyList();
     }
 }
